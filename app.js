eba9d98679c0a6a0e5ef7d8b5ec752b47c50b138
xkong-study <994885218@qq.com>
2023-01-18 23:37:40 +0000

    尝试

--- app.js
+++ app.js
@@ -623,11 +623,11 @@ app.set('view engine','pug')
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'pug');
 //
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 //
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);
