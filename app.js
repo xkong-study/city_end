b3f75580f43b94922ca20668d3258413b5742252
xkong-study <994885218@qq.com>
2023-01-19 00:01:12 +0000

    尝试

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.all("*",function(req,res,next){
   res.header("Access-Control-Allow-Origin","http://localhost:3004");
   res.header("Access-Control-Allow-Origin","https://xkong-study.github.io/urban/");
   res.header("Access-Control-Allow-Origin","https://maps.googleapis.com/maps/api/place");
+  res.header("Access-Control-Allow-Origin","https://cityengine.onrender.com");
   //允许的header类型
   res.header("Access-Control-Allow-Headers","content-type");
   //跨域允许的请求方式
@@ -633,19 +634,19 @@ app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 //
 // // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-// //
-// // // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+//
+// // error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 module.exports = app;
