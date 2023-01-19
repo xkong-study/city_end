0496cd4a535137a07ee67b531fc8dda0fa2a00ae
xkong-study <994885218@qq.com>
2023-01-19 00:14:13 +0000

    尝试

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mysql = require('mysql');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -631,7 +631,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //
 app.use('/index', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 //
 // // catch 404 and forward to error handler
 app.use(function(req, res, next) {
