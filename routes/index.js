a39544164f01f91fa421eca8add865f3913e2ef7
xkong-study <994885218@qq.com>
2023-01-19 00:05:29 +0000

    尝试

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 // 显示全部数据
-// router.get('/api/all', function(req, res, next) {
-//   res.send(str);
-// });
+router.get('/api/All', function(req, res, next) {
+  res.send(str);
+});
 
 
 module.exports = router;
